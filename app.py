9600f954285b8f355459ec5ed36141a08ae66979
PoorviAngadi <161704824+PoorviAngadi@users.noreply.github.com>
2026-05-28 12:11:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ responses = {
     "devops": "DevOps combines development and operations.",
     "docker": "Docker is used for containerization.",
     "kubernetes": "Kubernetes manages containers.",
-    "bye": "Goodbye!See you",
+    "bye": "Goodbye!Have a great day",
     "python": "Python is used in AI and DevOps."
     
 }
