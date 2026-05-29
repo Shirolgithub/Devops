316cf2b50b0376deaf4da13eece620f656e85a76
ateeqmujawar <161580637+ateeqmujawar@users.noreply.github.com>
2026-05-29 12:31:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ responses = {
     "devops": "DevOps combines development and operations.",
     "docker": "Docker is used for containerization.",
     "kubernetes": "Kubernetes manages containers.",
-    "bye": "Bye.Have  great day",
+    "bye": "Bye.bye",
     "python": "Python is used in AI and DevOps."
     
 }
