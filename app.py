c0c806486d3f97c701e411c9a970a1376d66a100
manojkambali <manojkambali99@gmail.com>
2026-06-01 11:06:30 +0530

    Updated chatbot feature

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ responses = {
     "devops": "DevOps combines development and operations.",
     "docker": "Docker is used for containerization.",
     "kubernetes": "Kubernetes manages containers.",
-    "bye": "Bye.bye.Updated by Ateeq",
+    "bye": "Bye.bye.Updated by Manoj",
     "python": "Python is used in AI and DevOps."
     
 }
